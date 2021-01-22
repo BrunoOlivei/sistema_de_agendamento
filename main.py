c0956bc886417f7b307098cd3a32114eb14bb213
BrunoOlivei <bruoli3@gmail.com>
2021-01-22 10:48:23 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Sistema de agendamento")
+print("Bem vindo ao agendamento da Dra. Giovana de Moraes")
+
+nome = input("Digite o nome completo do(a) paciente: ")
+nascimento = input("Digite a data de nascimento do(a) paciente (xx/xx/xx): ")
+convenio = input("Digite o convênio do(a) paciente: ")
+email = input("Digite um e-mail para contato: ")
+
